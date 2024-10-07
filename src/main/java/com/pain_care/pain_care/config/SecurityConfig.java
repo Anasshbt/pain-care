27f774917bac4a7dd729981d9d1bcacdbdab86ba
notMe <ahmed.amine.belaroussia@gmail.com>
2024-10-07 15:23:30 +0100

    Update SecurityConfig.java

--- src/main/java/com/pain_care/pain_care/config/SecurityConfig.java
+++ src/main/java/com/pain_care/pain_care/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers("/", "/assets/**", "/css/**", "/js/**", "/images/**").permitAll()
+                                .requestMatchers("/", "/assets/**", "/css/**", "/js/**", "/images/**", "/blog/**").permitAll() // Allow public access to blog
                                 .requestMatchers("/users/register").permitAll()
                                 .anyRequest().authenticated()
 
